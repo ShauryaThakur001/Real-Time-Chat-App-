94837b9db08e8b68b046059db2cc96d1e627337f
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-03-16 21:27:50 +0530

    Profiles Showed With StreamBuilder

--- lib/Screens/Home Screen/homeScreen.dart
+++ lib/Screens/Home Screen/homeScreen.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Firebase/Auth/EmailPasswordLogin.dart';
+import 'package:flutter_application_1/Firebase/FireStore/fireStore.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,15 +11,118 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchCoontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 7),
+          child: CircleAvatar(
+            radius: 18,
+            backgroundColor: Colors.blue.shade50,
+            child: Icon(Icons.person, color: Colors.blue.shade600, size: 30),
+          ),
+        ),
+        title: Text("Messages"),
         actions: [
-          IconButton(onPressed: (){
-            Emailpasswordlogin().logOut();
-          }, icon: Icon(Icons.logout))
+          CircleAvatar(
+            radius: 22,
+            backgroundColor: Colors.blue.shade50,
+            child: Icon(Icons.camera_alt, color: Colors.blue.shade600),
+          ),
+          SizedBox(width: 8),
+          CircleAvatar(
+            radius: 22,
+            backgroundColor: Colors.blue.shade50,
+            child: Icon(Icons.edit_document, color: Colors.blue.shade600),
+          ),
+          IconButton(
+            onPressed: () {
+              Emailpasswordlogin().logOut();
+            },
+            icon: Icon(Icons.logout),
+          ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: searchCoontroller,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, color: Colors.grey.shade600),
+                hintText: "Search conversations",
+                hintStyle: TextStyle(color: Colors.grey.shade600),
+                border: OutlineInputBorder(borderSide: BorderSide.none),
+                fillColor: Color(0xFFF0F8FF),
+                filled: true,
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection("users")
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  var users = snapshot.data!.docs;
+
+                  return ListView.separated(
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      var user = users[index];
+
+                      return userChat(
+                        name: user["name"],
+                        imageUrl: user["photoUrl"],
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return Divider(
+                        thickness: 0.6,
+                        color: Colors.grey.shade500,
+                        // indent: 70,
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class userChat extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+  const userChat({super.key, required this.name, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 0),
+      child: Container(
+        // height: 100,
+        width: double.infinity,
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25,
+            backgroundImage: NetworkImage(imageUrl),
+          ),
+          title: Text(name, style: TextStyle(fontSize: 19)),
+        ),
       ),
     );
   }
