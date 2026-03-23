38fee0f4b9bb5791e9986865e21ed4a2d96498ed
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-03-23 21:14:24 +0530

    Chat Id Screen Corrected

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Screens/Bottom%20Bar/bottomBar.dart';
+import 'package:flutter_application_1/Screens/SplashScreen/splashScreen.dart';
 import 'package:flutter_application_1/firebase_options.dart';
 
 void main() async {
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.blue),
       ),
-      home: BottomNavBar(),
+      home: SplashScreen(),
     );
   }
 }
